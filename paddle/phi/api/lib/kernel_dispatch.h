4653c5ef28fd0a4b8f531ebd0bd9678acccae5df
ZibinGuo <1009134431@qq.com>
2023-06-08 03:35:40 +0000

    Adapt Paddle to the commit f276f5 and add the functionality to ignore precision checks on the outputs mean_out, variance_out, saved_mean, saved_variance in the batch_norm kernel when global_stats=true.

--- paddle/phi/api/lib/kernel_dispatch.h
+++ paddle/phi/api/lib/kernel_dispatch.h
@@ -27,6 +27,7 @@ limitations under the License. */
 #include "paddle/phi/core/selected_rows.h"
 #include "paddle/phi/core/sparse_coo_tensor.h"
 #include "paddle/phi/core/sparse_csr_tensor.h"
+#include "glog/logging.h"
 
 #ifdef PADDLE_WITH_DISTRIBUTE
 #include "paddle/phi/core/distributed/auto_parallel/dist_tensor.h"
